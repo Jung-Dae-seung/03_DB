3a575d76b6ad966a55143a5ae1795763b7de0fcf
Jung-Dae_seung <eotmd1213@naver.com>
2024-10-11 17:12:37 +0900

    2024-10-11

--- 사용자계정script.sql
+++ 사용자계정script.sql
@@ -1,15 +1,27 @@
 -- 한 줄 주석
-/* 
+/*
  * 범위주석
  * 
- */
+ * */
 
 
+-- 선택한 SQL 수행 : 구문에 커서 두고 CTRL + ENTER
+-- 전체 SQL 수행 : alt + x
+-- SQL(Structured Query Language , 구조적 질의 언어) : 
+-- 데이터베이스와 상호작용을 하기 위해 사용하는 표준 언어
+-- 데이터의 조회, 삽입, 수정, 삭제 등 
+
+-- 새로운 사용자 계정 생성 (sys : 최고 관리자 계정) ** 중요
 ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
--- 선택한 SQL 수행 : 구문 커서 두시고 CTRL + ENTER
+-- 11G 이전 문법 사용 허용
 
-CREATE USER kh_jds IDENTIFIED BY kh1234;
+CREATE USER kh_cmh IDENTIFIED BY kh1234;
+-- 계정 생성 구문 (kh_cmh : ID / kh1234 : Password) 
 
-GRANT RESOURCE, CONNECT TO kh_jds;
+GRANT RESOURCE, CONNECT TO kh_cmh; -- ** 중요
+-- 사용자 계정 권한 부여 설정
+-- RESOURCE : 테이블이나 인덱스 같은 DB 객체를 생성할 권한
+-- CONNECT : DB에 연결하고 로그인할 수 있는 권한
 
-ALTER USER kh_jds DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+ALTER USER kh_cmh DEFAULT TABLESPACE SYSTEM QUOTA UNLIMITED ON SYSTEM;
+-- 객체가 생성될 수 있는 공간 할당량 무제한 지정
