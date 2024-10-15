451ab9d7932a72d6cda86ccde007114db0244241
Jung-Dae_seung <eotmd1213@naver.com>
2024-10-15 17:36:54 +0900

    2024-10-15

--- workbook/SQL02_SELECT(Function).sql
+++ workbook/SQL02_SELECT(Function).sql
@@ -63,3 +63,67 @@ FROM DUAL;
 SELECT STUDENT_NO, STUDENT_NAME
 FROM TB_STUDENT
 WHERE SUBSTR(STUDENT_NO, 0, 1) != 'A'; 
+
+
+-- 9. 학번이 A517178 인 한아름 학생의 학점 총 평점을 구하는 SQL 문을 작성하시오.
+--    단, 이때 출력 화면의 헤더는 "평점" 이라고 찍히게 하고, 점수는 반올림하여
+--    소주점 이하 한자리까지만 표시한다.
+SELECT ROUND(AVG(POINT),1) 평점
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A517178';
+
+
+-- 10. 학과별 학생수를 구하여 
+--     "학과번호", "학생수(명)" 의 형태로 헤더를 만들어
+--     결과값이 출력되도록 하시오
+SELECT DEPARTMENT_NO 학과번호, COUNT(*) "학생수(명)"
+FROM TB_STUDENT
+GROUP BY DEPARTMENT_NO 
+ORDER BY DEPARTMENT_NO;
+
+
+-- 11. 지도 교수를 배정받지 못한 학생의 수는 몇 명 정도 되는 알아내는
+--     SQL 문을 작성하시오.
+SELECT COUNT(*)
+FROM TB_STUDENT
+WHERE COACH_PROFESSOR_NO IS NULL;
+
+
+-- 12. 학번이 A112113인 김고운 학생의 년도 별 평점을 구하는 SQL문을 작성하시오.
+--     이때 출력 화면의 헤더는 "년도", "년도 별 평점" 이라고 찍히게 하고,
+--     점수는 반올림하여 소수점 이하 한 자리까지만 표시한다.
+SELECT SUBSTR(TERM_NO, 1, 4) 년도, ROUND(AVG(POINT),1) "년도 별 평점"
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY SUBSTR(TERM_NO, 1, 4)
+ORDER BY 1;
+
+
+-- 13. 학과 별 휴학생 수를 파악하고자 한다.
+--     학과 번호화 휴학생 수를 표시하는 SQL 문장을 작성하시오.
+SELECT DEPARTMENT_NO 학과코드명, COUNT(*) "휴학생 수"
+FROM TB_STUDENT 
+WHERE ABSENCE_YN = 'Y'
+GROUP BY DEPARTMENT_NO
+ORDER BY DEPARTMENT_NO;
+
+
+-- 14. 춘 대학교에 다니는 동명이인 학생들의 이름을 찾고자 한다.
+--     어떤 SQL 문장을 사용하면 가능하겠는가?
+SELECT STUDENT_NAME 동일이름, COUNT(*) AS "동명인 수"
+FROM TB_STUDENT
+GROUP BY STUDENT_NAME
+HAVING COUNT(*) > 1
+ORDER BY STUDENT_NAME;
+
+
+-- 15. 학번이 A112113 인 김고운 학생의 년도, 학기 별 평점과
+--     년도 별 누적 평점, 총 평점을 구하는 SQL 문을 작성하시오.
+--     (단, 평점은 소수점 1자리까지만 반올림하여 표시한다.)
+SELECT SUBSTR(TERM_NO, 1, 4) 년도, 
+  		 SUBSTR(TERM_NO, 5, 6) 학기,
+  		 ROUND(AVG(POINT), 1) 평점
+FROM TB_GRADE
+WHERE STUDENT_NO = 'A112113'
+GROUP BY ROLLUP (SUBSTR(TERM_NO, 1, 4), SUBSTR(TERM_NO, 5, 6))
+ORDER BY 1;
